8b7002ce26c9e24903799a0d8239c531f11e076a
Jacksons Laptop <jacksontallent48@gmail.com>
2022-10-11 10:55:50 -0400

    Refactor, removed testing tools, cleaned up comments

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,7 @@
 let initHour = moment();
-//todo check LS on startup to see notes
-// 
 
 
+//Checks if the content from local storage is from the same date then populates input fields with the saved text
 function loadFromLS() {
     let momentobj = moment(localStorage.getItem("momentobj"));
     if (momentobj.isSame(initHour, "date")) {
@@ -10,16 +9,14 @@ function loadFromLS() {
             $(`#${i}i`).val(localStorage.getItem(i));
         }
     }
-    
 }
 
+//Saves the selected hours text area
 function saveToLS(hour, content) {
-    console.log(`Content: ${content}`);
-    console.log(`~~~~`);
-    console.log(`Hour: ${hour}`);
     localStorage.setItem(hour, content);
 }
 
+//Colors timeblocks depending on the time
 function drawSchedule() {
     const twelve = (number) => {return number > 12 ? number-12 : number}
     for (i=9; i<18; i++){
@@ -33,15 +30,14 @@ function drawSchedule() {
         }
         else if (initHour.hour() === i) {
             timeblock.addClass("present");
-            console.log("hi")
         }
     }
 }
 
+//Tells the current date in the jumbotron
 $("#currentDay").text(moment().format("[Today is] dddd, MMMM Do YYYY"));
+
 loadFromLS();
+//Updates the momentobj that is compared to the current time.
 localStorage.setItem("momentobj", initHour)
 drawSchedule();
-
-console.log(initHour.format());
-console.log(moment(14,"HH"));
